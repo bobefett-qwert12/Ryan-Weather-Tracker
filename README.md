ca7f666d6fea4702eb434cec8f0e30c4170505d9
Ryan Perkins <perkins.ryanhanna@gmail.com>
2022-10-19 15:43:07 -0600

    API calls working, added more html, added bootstrap

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ This is a simple app for tracking the weather wherever you may be!
 ## Link to Deployed Application
 
 Find the deployed application [here.](https://bobefett-qwert12.github.io/Ryan-Weather-Tracker/)
+
+### Notes for moi
+
+https://openweathermap.org/forecast5
+https://openweathermap.org/api/geocoding-api
+https://getbootstrap.com/docs/4.6/utilities/colors/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
-var apiKey = "";
+const apiKey = "b926f2eadf16d646b404c4309d865c6f";
 var weatherStats;
-var nameInputEl = document.getElementsByClassName("nameInput");
-var searchButtonEl = document.getElementsByClassName("search");
+var nameInputEl = document.getElementById("nameInput");
+var searchButtonEl = document.getElementById("search");
 
 function getApi(requestUrl) {
   fetch(requestUrl)
     .then(function (response) {
-      console.log(response);
-      if (response.status === 200) {
-        responseText.textContent = response.status;
-      }
-      return response.json();
+      var json = response.json();
+      console.log(json);
+      return json;
   });
 }
 
 function getRequestUrl(cityUrl){
   var cityStats = getApi(cityUrl);
-  var lat = cityStats.lat;
-  var lon = cityStats.lon;
+  var lat;
+  var lon;
+  for(let i in cityStats){
+    if(Array.isArray(i)){
+      lat = i[0].lat;
+      lon = i[0].lon;
+    }
+  }
   var requestUrl = ('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey);
   weatherStats = getApi(requestUrl);
-  console.log(weatherStats);
+  //console.log(weatherStats);
   //var temperature = weatherStats.temp;
   //var wind = weatherStats.speed;
   //var humidity = weatherStats.wind;
@@ -29,9 +33,16 @@ function getRequestUrl(cityUrl){
 }
 
 function getCityUrl(){
-  var cityName = nameInputEl.innerHTML;
-  var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
+  //var cityName = nameInputEl.innerHTML;
+  var cityName = "provo";
+  var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apiKey;
   getRequestUrl(cityUrl);
+  for(var i = 0; i < 20; i++){
+    if(localStorage.getItem(i) != null && localStorage.getItem(i) != ""){
+      localStorage.setItem(i, cityName);
+      return null;
+    }
+  }
 }
 
-searchButtonEl.addEventListener("click", getCityUrl());
+searchButtonEl.addEventListener("click", getCityUrl);
